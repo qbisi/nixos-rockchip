958405fb99567690d4a7c3769d0bb97ec4a17aa1
qbisi <qbisicwate@gmail.com>
2024-07-01 04:05:57 +0800

    add new kernel package with buildLinux

--- pkgs/top-level.nix
+++ pkgs/top-level.nix
@@ -3,6 +3,7 @@
   brcmfmac_sdio-firmware = pkgs.callPackage ./brcmfmac_sdio-firmware.nix { };
   linux-aarch64-7ji-6_9 = pkgs.callPackage ./linux-aarch64-7ji-6_9 { };
   linux-aarch64-rockchip-bsp6_1= pkgs.callPackage ./linux-aarch64-rockchip-bsp6_1 { };
+  linux-aarch64-rockchip-bsp= pkgs.callPackage ./linux-aarch64-rockchip-bsp { };
 } 
 // (pkgs.callPackage ./uboot.nix { }) 
 // (pkgs.callPackage ./compileDTS.nix { })
