ece8ef93871e112c21531af0a494f8532e405ab5
qbisi <qbisicwate@gmail.com>
2024-08-07 05:38:15 +0800

    pkgs: Add mali-g610-firmware.

--- pkgs/top-level.nix
+++ pkgs/top-level.nix
@@ -8,6 +8,8 @@
   mpp = pkgs.callPackage ./mpp.nix {};
   librga-multi = pkgs.callPackage ./librga-multi.nix {};
   ffmpeg-rockchip = pkgs.callPackage ./ffmpeg-rockchip.nix {};
+  mali-g610-firmware = pkgs.callPackage ./mali-g610-firmware.nix {};
+  mali-panthor-g610-firmware = pkgs.callPackage ./mali-panthor-g610-firmware.nix {};
 } 
 // (pkgs.callPackage ./kernels/linux-aarch64-rkbsp-joshua.nix { }) 
 // (pkgs.callPackage ./uboot.nix { }) 
