a2257100c30dc2b75b627895f45c099d421558ea
qbisi <qbisicwate@gmail.com>
2024-08-07 05:36:35 +0800

    pkgs:kernel: rkbsp kernel panthor support.

--- pkgs/kernels/linux-aarch64-rkbsp-joshua.nix
+++ pkgs/kernels/linux-aarch64-rkbsp-joshua.nix
@@ -8,7 +8,16 @@ let
     rev = "Ubuntu-rockchip-6.1.0-1020.20";
     hash = "sha256-m8ZpkJvU1EKkDfTmOmzFbD/uFL1nxep/So4VqQRYlu0=";
   };
+  _panthor_base = "aa54fa4e0712616d44f2c2f312ecc35c0827833d";
+  _panthor_branch = "rk-6.1-rkr3-panthor";
   kernelPatches = [
+    {
+      name = "panthor";
+      patch = fetchurl {
+        url = "https://github.com/hbiyik/linux/compare/${_panthor_base}...${_panthor_branch}.patch";
+        hash = "sha256-fDrl634LFH4Ou6Ky3dGit5WqASUvPlr9ngdb6a/wsss=";
+      };
+    }
     {
       name = "link_defconfig";
       patch =./link_defconfig.patch;
