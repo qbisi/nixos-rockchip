4b5758102484746689f6215e6aed06ab47132a1f
qbisi <qbisicwate@gmail.com>
2024-08-07 09:16:25 +0800

    modules: modulize disko

--- modules/disko.nix
+++ modules/disko.nix
@@ -1,81 +1,102 @@
+{ config, options, lib, ... }:
+with lib;
 {
-    disko = {
-    enableConfig = true;
-    memSize = 4096;
-    devices = {
-      disk.main = {
-        imageSize = "2G";
-        device = "/dev/disk/by-diskseq/1";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP-sdhci = {
-              name = "ESP";
-              start = "16M";
-              end = "20M";
-              type = "EF00";
-              priority = 1;
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot/efi";
-                mountOptions = [ "fmask=0077" "dmask=0077" ];
-              };
-            };
-            nix-sdhci = {
-              size = "100%";
-              # arm64-root
-              type = "8305";
-              content = {
-                type = "btrfs";
-                extraArgs = [ "-f" ];
-                subvolumes = {
-                  "/" = {
-                    mountOptions = [ "noatime" ];
-                    mountpoint = "/.btrfs_root";
+  options = {
+      disko.devicetype = mkOption {
+        default = "sdhci";
+        type = types.enum ["sdhci" "sdmmc" "nvme" "sata" "usb"];
+      };
+      disko.u-boot.enable = mkOption {
+        default = (config.disko.devicetype == "sdhci" || 
+                   config.disko.devicetype == "sdmmc") ;
+        type = types.bool;
+        description = ''
+          Whether to enable the u-boot partion.
+        '';
+      };
+  };
+
+  config =
+    {
+      disko = {
+        enableConfig = true;
+        memSize = 4096;
+        devices = {
+          disk.main = {
+            imageSize = "2G";
+            device = "/dev/disk/by-diskseq/1";
+            type = "disk";
+            content = {
+              type = "gpt";
+              partitions = {
+                ESP = {
+                  name = "ESP-${config.disko.devicetype}";
+                  start = if config.disko.u-boot.enable then "16M" else "1M";
+                  size = "4M";
+                  type = "EF00";
+                  priority = 1;
+                  content = {
+                    type = "filesystem";
+                    format = "vfat";
+                    mountpoint = "/boot/efi";
+                    mountOptions = [ "fmask=0077" "dmask=0077" ];
                   };
-                  "/@" = {
-                    mountOptions = [ "compress=zstd" "noatime" ];
-                    mountpoint = "/";
+                };
+                nix = {
+                  name = "nix-${config.disko.devicetype}";
+                  size = "100%";
+                  # arm64-root
+                  type = "8305";
+                  content = {
+                    type = "btrfs";
+                    extraArgs = [ "-f" ];
+                    subvolumes = {
+                      "/" = {
+                        mountOptions = [ "noatime" ];
+                        mountpoint = "/.btrfs_root";
+                      };
+                      "/@" = {
+                        mountOptions = [ "compress=zstd" "noatime" ];
+                        mountpoint = "/";
+                      };
+                      "/@var" = {
+                        mountOptions = [ "compress=zstd" "noatime" ];
+                        mountpoint = "/var";
+                      };
+                      "/@home" = {
+                        mountOptions = [ "compress=zstd" "noatime" ];
+                        mountpoint = "/home";
+                      };
+                      "/@swap" = {
+                        mountpoint = "/swap";
+                        mountOptions = [ "x-initrd.mount" "noatime" ];
+                      };
+                      # "/@persistent" = {
+                      #   mountOptions = [ "compress=zstd" "noatime" "x-initrd.mount"];
+                      #   mountpoint = "/nix/persistent";
+                      # };
+                    };
                   };
-                  "/@var" = {
-                    mountOptions = [ "compress=zstd" "noatime" ];
-                    mountpoint = "/var";
-                  };
-                  "/@home" = {
-                    mountOptions = [ "compress=zstd" "noatime" ];
-                    mountpoint = "/home";
-                  };
-                  "/@swap" = {
-                    mountpoint = "/swap";
-                    mountOptions = [ "x-initrd.mount" "noatime" ];
-                  };
-                  # "/@persistent" = {
-                  #   mountOptions = [ "compress=zstd" "noatime" "x-initrd.mount"];
-                  #   mountpoint = "/nix/persistent";
-                  # };
                 };
               };
             };
           };
+          # nodev."/etc" = {
+          #   fsType = "tmpfs";
+          #   mountOptions = ["relatime" "mode=755" "nosuid" "nodev"];
+          # };
         };
       };
-      # nodev."/etc" = {
-      #   fsType = "tmpfs";
-      #   mountOptions = ["relatime" "mode=755" "nosuid" "nodev"];
+      # fileSystems."/nix/persistent" = {
+      #   device = "/dev/disk/by-partlabel/disk-main-nix";
+      #   neededForBoot = true;
+      #   fsType = "btrfs";
+      #   options = [ "subvol=@persistent" "compress=zstd" "noatime"];
       # };
+      # swapDevices = [
+      #   {
+      #     device = "/swap/swapfile";
+      #   }
+      # ];
     };
-  };
-  # fileSystems."/nix/persistent" = {
-  #   device = "/dev/disk/by-partlabel/disk-main-nix";
-  #   neededForBoot = true;
-  #   fsType = "btrfs";
-  #   options = [ "subvol=@persistent" "compress=zstd" "noatime"];
-  # };
-  # swapDevices = [
-  #   {
-  #     device = "/swap/swapfile";
-  #   }
-  # ];
 }
