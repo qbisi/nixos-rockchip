5c99db8e23f50fdcd1d6ea9b8f63809435d1f7c7
qbisi <qbisicwate@gmail.com>
2024-08-10 07:55:46 +0800

    pkgs: Refactor ubootRK3399 pkgs.

--- overlay/buildUBoot.nix
+++ overlay/buildUBoot.nix
@@ -1,8 +1,8 @@
 final: prev:
 let
   dts = prev.lib.fileset.toSource {
-    root = ../dts;
-    fileset = ../dts;
+    root = ../dts/u-boot;
+    fileset = ../dts/u-boot;
   };
   configfile = prev.lib.fileset.toSource {
     root = ../configs;
@@ -14,6 +14,7 @@ in
     passAsFile = [ "extraConfig" ];
     prePatch = ''
       install -m 666 ${dts}/* ./arch/arm/dts/
+      install -m 666 ${dts}/* ./dts/upstream/src/arm64/rockchip/
       install -m 666 ${configfile}/* ./configs/
       { echo "";cat $extraConfigPath; } >> ./configs/${defconfig}
     '';
--- pkgs/u-boot/default.nix
+++ pkgs/u-boot/default.nix
@@ -9,31 +9,95 @@
 , buildUBoot
 }:
 let
-  ubootRK3399 = { Device_Tree, DDR? "800MHz", ... }: (buildUBoot {
+  ubootRK3399 =
+    { defconfig
+    , deviceTree ? null
+    , ROCKCHIP_TPL ? null
+    , manufacturer ? null
+    , product ? null
+    , version ? null
+    , family ? null
+    , smbiosSupport ? true
+    , videoSupport ? true
+    , efiSupport ? true
+    , usbSupport ? true
+    , keyboardSupport ? true
+    , extraConfig ? ""
+    , extraPatches ? [ ]
+    , ...
+    }@args:
+      assert keyboardSupport -> usbSupport;
+      assert (!isNull manufacturer) -> smbiosSupport;
+      assert (!isNull product) -> smbiosSupport;
+      assert (!isNull version) -> smbiosSupport;
+      assert (!isNull family) -> smbiosSupport;
+      (buildUBoot ({
+        inherit defconfig;
+        extraPatches = [ ./add-smbios-config.patch ./rk3399-devicetree-display-subsystem-add-label.patch ]
+          ++ extraPatches;
+        extraConfig = lib.optionalString (!isNull deviceTree) ''
+          CONFIG_DEFAULT_DEVICE_TREE="${deviceTree}"
+          CONFIG_DEFAULT_FDT_FILE="${deviceTree}.dtb"
+        '' + lib.optionalString smbiosSupport (''
+          CONFIG_SYSINFO=y
+          CONFIG_SYSINFO_SMBIOS=y
+        '' + lib.optionalString (!isNull manufacturer) ''
+          CONFIG_SYSINFO_SMBIOS_MANUFACTURER="${manufacturer}"
+        '' + lib.optionalString (!isNull product) ''
+          CONFIG_SYSINFO_SMBIOS_PRODUCT="${product}"
+        '' + lib.optionalString (!isNull version) ''
+          CONFIG_SYSINFO_SMBIOS_VERSION="${version}"
+        '' + lib.optionalString (!isNull family) ''
+          CONFIG_SYSINFO_SMBIOS_FAMILY="${family}"
+        '') + lib.optionalString (!isNull ROCKCHIP_TPL) ''
+          CONFIG_ROCKCHIP_EXTERNAL_TPL=y
+        '' + lib.optionalString videoSupport ''
+          CONFIG_VIDEO=y
+          CONFIG_DISPLAY=y
+          CONFIG_VIDEO_ROCKCHIP=y
+          CONFIG_DISPLAY_ROCKCHIP_HDMI=y
+        '' + lib.optionalString efiSupport ''
+          CONFIG_BOOTSTD_FULL=y
+          CONFIG_BOOTCOMMAND="bootmenu"
+          CONFIG_BOOTMENU_DISABLE_UBOOT_CONSOLE=y
+          CONFIG_CMD_BOOTMENU=y
+          CONFIG_CMD_EFICONFIG=y
+          CONFIG_USE_PREBOOT=y
+          CONFIG_PREBOOT="usb start; setenv bootmenu_0 UEFI Boot Manager=bootefi bootmgr; setenv bootmenu_1 UEFI Maintenance Menu=eficonfig"
+        '' + lib.optionalString usbSupport ''
+          CONFIG_PHY_ROCKCHIP_INNO_USB2=y
+          CONFIG_PHY_ROCKCHIP_TYPEC=y
+          CONFIG_USB=y
+          CONFIG_USB_XHCI_HCD=y
+          CONFIG_USB_XHCI_DWC3=y
+          CONFIG_USB_EHCI_HCD=y
+          CONFIG_USB_EHCI_GENERIC=y
+          CONFIG_USB_OHCI_HCD=y
+          CONFIG_USB_OHCI_GENERIC=y
+        '' + lib.optionalString keyboardSupport ''
+          CONFIG_USB_KEYBOARD=y
+          CONFIG_SYS_USB_EVENT_POLL_VIA_CONTROL_EP=y
+        '' + extraConfig;
 
-    defconfig = "rk3399_defconfig";
-
-    extraConfig = ''
-      CONFIG_DEFAULT_DEVICE_TREE="${Device_Tree}"
-      CONFIG_DEFAULT_FDT_FILE="${Device_Tree}.dtb"
-    '';
-
-    BL31 = "${armTrustedFirmwareRK3399}/bl31.elf";
-    ROCKCHIP_TPL = "${rkbin}/bin/rk33/rk3399_ddr_${DDR}_v1.30.bin";
-
-    extraMeta = {
-      platforms = [ "aarch64-linux" ];
-    };
-    filesToInstall = [ "u-boot.itb" "idbloader.img" "u-boot-rockchip.bin" ];
-  });
+        extraMeta = {
+          platforms = [ "aarch64-linux" ];
+        };
+        filesToInstall = [ "u-boot.itb" "idbloader.img" "u-boot-rockchip.bin" ];
+      } // builtins.removeAttrs args [ "extraConfig" "extraPatches" ]));
 in
 {
   inherit ubootRK3399;
-  uboot-fine3399 = ubootRK3399 { Device_Tree = "rk3399-fine3399"; };
-  uboot-bozz-sw799 = ubootRK3399 { Device_Tree = "rk3399-bozz-sw799"; DDR="800MHz";};
-  uboot-cdhx-rb30 = ubootRK3399 { Device_Tree = "rk3399-cdhx-rb30"; };
-  uboot-eaio-3399j = ubootRK3399 { Device_Tree = "rk3399-eaio-3399j"; };
-  uboot-nanopi-m4-2gb = ubootRK3399 { Device_Tree = "rk3399-nanopi-m4-2gb"; };
+  uboot-bozz-sw799 = ubootRK3399 {
+    defconfig = "evb-rk3399_defconfig";
+    deviceTree = "rockchip/rk3399-bozz-sw799a-5g";
+    manufacturer = "Bozz Tech";
+    product = "Bozz SW799A";
+    version = "5G";
+    family = "Rockchip/RK3399";
+    BL31 = "${armTrustedFirmwareRK3399}/bl31.elf";
+    ROCKCHIP_TPL = "${rkbin}/bin/rk33/rk3399_ddr_800MHz_v1.30.bin";
+  };
+
   uboottest = buildUBoot {
 
     version = "2024.04";
@@ -46,7 +110,7 @@ in
     };
 
     defconfig = "bozz-sw799-rk3399_defconfig";
-    
+
     # extraConfig = ''
     #   CONFIG_DEFAULT_DEVICE_TREE="rk3399-aio-3399j"
     #   CONFIG_DEFAULT_FDT_FILE="rk3399-aio-3399j.dtb"
